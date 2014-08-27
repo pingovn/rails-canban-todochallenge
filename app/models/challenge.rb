6e2f64abd4dce8f38a7b62877d6948899503d433
Quy Doan <quydoantran@gmail.com>
2014-08-27 18:07:46 +0700

    Add validation and association to challenge

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,2 +1,15 @@
 class Challenge < ActiveRecord::Base
+  belongs_to :creator, :foreign_key => :creator_id, :class_name => 'User'
+  validates :name, :starts_on, :presence => true
+  validates :name, :length => {:minimum => 10, :maximum => 50}
+  validates :description, :length => {:maximum => 500}
+  validate :starts_on_is_in_future, :on => :create
+
+
+  protected
+  def starts_on_is_in_future
+    if starts_on && starts_on <= Date.today
+      errors.add(:starts_on, "should be in the future")
+    end
+  end
 end
